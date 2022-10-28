138b5a7677e139afa64f3a25f0ce41472531ccf6
Joshua Camara <redawgts@gmail.com>
2022-10-27 23:29:51 -0400

    Circle Jump: A Godot Mobile Game (Part 2)
    https://youtu.be/ahsFSeDbG84

--- Objects/Circle.gd
+++ Objects/Circle.gd
@@ -1,23 +1,33 @@
 extends Area2D
 
+export(int) var radius := 100
+export(float) var rotation_speed := PI
+
 onready var collision_shape := $CollisionShape2D
 onready var sprite := $Sprite
 onready var pivot := $Pivot
 onready var orbit_position := $Pivot/OrbitPosition
+onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-export(int) var radius := 100
-export(float) var rotation_speed := PI
 
-func _ready() -> void:
-	init()
-
-func init(_radius = radius):
+func init(_position, _radius = radius):
+	position = _position
 	radius = _radius
 	collision_shape.shape = collision_shape.shape.duplicate()
 	collision_shape.shape.radius = radius
 	var img_size = sprite.texture.get_size().x / 2
 	sprite.scale = Vector2(1,1) * radius / img_size
 	orbit_position.position.x = radius + 25
+	rotation_speed *= pow(-1, randi() % 2)
 
 func _process(delta: float) -> void:
 	pivot.rotation += rotation_speed * delta
+
+func implode():
+	animation_player.play("implode")
+	yield(animation_player, "animation_finished")
+	queue_free()
+
+func capture():
+	animation_player.play("capture")
+	yield(animation_player, "animation_finished")
---
+++ main.gd
@@ -0,0 +1,33 @@
+extends Node
+
+
+var Circle = preload("res://objects/circle.tscn")
+var Jumper = preload("res://objects/jumper.tscn")
+
+var player
+
+func _ready() -> void:
+	randomize()
+	new_game()
+
+func new_game():
+	$Camera2D.position = $StartPosition.position
+	player = Jumper.instance()
+	player.position = $StartPosition.position
+	add_child(player)
+	player.connect("captured", self, "_on_jumper_captured")
+	spawn_circle($StartPosition.position)
+
+func spawn_circle(_position = null):
+	var c = Circle.instance()
+	if !_position:
+		var x = rand_range(-150, 150)
+		var y = rand_range(-500, -400)
+		_position = player.target.position + Vector2(x, y)
+	add_child(c)
+	c.init(_position)
+
+func _on_jumper_captured(object):
+	$Camera2D.position = object.position
+	object.capture()
+	call_deferred("spawn_circle")
